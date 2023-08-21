6e8dee1f7712d540ee50cd595b8fb5e9021197d2
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-21 11:57:13 +0300

    crud lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,43 @@
 def create_an_empty_list():
-    return None
+    my_list = []
+    return my_list
+
 
 def create_a_list():
-    return None
+    my_list = ["shercie", "wesh", "memer", "sherciewesh"]
+    return my_list
 
-def add_element_to_end_of_list(l, element):
-    return None
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_end_of_list(my_list, memerwesh):
+    my_list.append(memerwesh)
+    return my_list
 
-def remove_element_from_end_of_list(l):
-    return None
 
-def remove_element_from_start_of_list(l):
-    return None
+def add_element_to_start_of_list(my_list, john):
+    my_list.insert(0, john)
+    return my_list
 
-def retrieve_first_element_from_list(l):
-    return None
 
-def retrieve_element_from_index(l, index):
-    return None
+def remove_element_from_end_of_list(my_list):
+    my_list.pop()
+    return my_list
 
-def retrieve_last_element_from_list(l):
-    return None
+
+def remove_element_from_start_of_list(my_list):
+    del my_list[0]
+    return my_list
+
+
+def retrieve_first_element_from_list(my_list):
+    my_list = my_list[0]
+    return my_list
+
+
+def retrieve_element_from_index(my_list, index):
+    my_list = my_list[2]
+    return my_list
+
+
+def retrieve_last_element_from_list(my_list):
+    my_list = my_list[-1]
+    return my_list
